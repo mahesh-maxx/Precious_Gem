a47134a7aeb1ba316ec250feab219bbea88ab808
Mahesh Jaipal <mahesh.max.kumar86@gmail.com>
2017-10-05 20:26:21 +0530

    added store controller

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 function () {
-    var app = angular.module('Gemstore',[]);
-    app.controller()
+    var app = angular.module('Gemstore',['store-directives']);
+    app.controller('StoreController',function(
+        {
+            this.products = gems;
+        }
+    )
+);
+    app.controller('GalleryController',function(
+        {
+
+        }
+    ));
 }
