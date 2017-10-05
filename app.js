d3fc5649bb47dacd1e594c20acd16fdf1895ae64
Mahesh Jaipal <mahesh.max.kumar86@gmail.com>
2017-10-05 20:41:08 +0530

    added gallery & review controller

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-function () {
-    var app = angular.module('Gemstore',['store-directives']);
-    app.controller('StoreController',function(
-        {
+
+    var app = angular.module('Gemstore', ['store-directives']);
+    app.controller('StoreController', function () {
             this.products = gems;
         }
-    )
-);
-    app.controller('GalleryController',function(
-        {
+    );
+    app.controller('GalleryController', function () {
+        this.imageIndex = 0;
+        this.changeImageIndex = function (image) {
+            this.imageIndex = image || 0;
+        };
+    });
 
-        }
-    ));
-}
+    app.controller('ReviewController', function () {
+        this.review = {};
+        this.addReview = function (product) {
+            this.product.createdOn = Date.now();
+            this.product.review.push(this.review);
+            this.review = {};
+        };
+    });
+
